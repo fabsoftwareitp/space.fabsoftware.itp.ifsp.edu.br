8f50d41a1367c56fb51bde342b2cfaaf2240e8f9
Vitor Gabriel de Aquino <95290697+Vitor-G-Aquino@users.noreply.github.com>
2022-09-21 19:02:05 -0300

    Tela de início e fullscreen

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const scoreEl = document.querySelector("#scoreEl");
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 var aceler_gamma = 0;
+var begingame = false;
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
-canvas.requestFullscreen();
 
 class Player {
   constructor() {
@@ -245,7 +245,7 @@ let frames = 0;
 let randomInterval = Math.floor(Math.random() * 500 + 500);
 let game = {
   over: false,
-  active: true,
+  active: false,
 };
 let score = 0;
 
@@ -275,20 +275,32 @@ if ('DeviceOrientationEvent' in window) {
 }
 
 //detectar toque na tela
+function endtouch() {
+  if (!game.active && !begingame){
+    begingame = true;
+    canvas.requestFullscreen();
+  }
+}
+document.querySelector("canvas").addEventListener("touchend", endtouch);
+
+var firsttime = true;
 function touchscreen() {
   if (game.over) return;
-  projectiles.push(
-    new Projectile({
-      position: {
-        x: player.position.x + player.width / 2,
-        y: player.position.y,
-      },
-      velocity: {
-        x: 0,
-        y: -10,
-      },
-    })
-  );
+  if (!firsttime){
+    projectiles.push(
+      new Projectile({
+        position: {
+          x: player.position.x + player.width / 2,
+          y: player.position.y,
+        },
+        velocity: {
+          x: 0,
+          y: -10,
+        },
+      })
+    );
+  }
+  firsttime = false;
 }
 document.querySelector("canvas").addEventListener("touchstart", touchscreen);
 
@@ -312,11 +324,39 @@ function createParticles({ object, colour, fades }) {
   }
 }
 
+function fillText(text, x, y, color, fontSize) {
+  if (typeof color !== 'undefined') c.fillStyle = color;
+  if (typeof fontSize !== 'undefined') c.font = fontSize + 'px Play';
+  c.fillText(text, x, y);
+}
+
+function fillCenteredText(text, x, y, color, fontSize) {
+  var metrics = c.measureText(text);
+  fillText(text, x - metrics.width/2, y, color, fontSize);
+}
+
+function fillBlinkingText(text, x, y, blinkFreq, color, fontSize) {
+  if (~~(0.5 + Date.now() / blinkFreq) % 2) {
+    fillCenteredText(text, x, y, color, fontSize);
+  }
+}
+
+function drawStartScreen() {
+  c.fillText
+  fillCenteredText("Space Invaders", canvas.width/2, canvas.height/2.75, '#FFFFFF', 36);
+  fillBlinkingText("Toque na tela para começar!", canvas.width/2, canvas.height/2, 500, '#FFFFFF', 36);
+}
+
+var lastTime = 0;
 function animate() {
-  if (!game.active) return;
-  requestAnimationFrame(animate);
+  var now = window.performance.now();
+  if (!game.active && begingame){
+    game.active = true;
+    begingame = false;
+  }
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
+  if (game.active){
   player.update();
   particles.forEach((particle, i) => {
     if (particle.position.y - particle.radius >= canvas.height) {
@@ -484,6 +524,11 @@ function animate() {
   }
 
   frames++;
+  }else{
+    drawStartScreen();
+  }
+  lastTime = now;
+  requestAnimationFrame(animate);
 }
 
 animate();
